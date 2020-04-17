f19342c67033716185a4d6b7170ab6447712bd1b
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-04-17 20:08:55 +0100

    js: binary search recursive

--- javascript/binary_search.js
+++ javascript/binary_search.js
@@ -25,3 +25,22 @@ function binary_search(arr, target) {
 
 console.log(binary_search([1, 2, 3, 4, 5], 6)); // -1
 console.log(binary_search([1, 2, 3, 4, 5], 5)); // 1
+
+function binary_search_recursive(arr, target, low, high) {
+    if (arr.length === 0) {
+        return -1
+    }
+    let mid = Math.floor((low + high) / 2);
+    if (low <= high) {
+        if (arr[mid] === target) {
+            return 1
+        } else if (target > arr[mid]) {
+            return binary_search_recursive(arr, target, mid+1, high)
+        } else {
+            return binary_search_recursive(arr, target,low,mid-1)
+        }
+    }
+    return -1
+}
+
+console.log(binary_search_recursive([1, 2, 3], 4,0,2))
